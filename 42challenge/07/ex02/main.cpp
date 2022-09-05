4e579338c149040e97e13311f9af00f78fdfed26
vctrubio <vctrubio@gmail.com>
2022-09-05 21:09:13 +0200

    casting issue rsolved

--- 42challenge/07/ex02/main.cpp
+++ 42challenge/07/ex02/main.cpp
@@ -4,16 +4,27 @@ int main()
 {
 	int size = 2;
 	Array<float> i(size);
-    // Array<int> tmp2 = static_cast<Array*>(&i); How to do this- fuck sake
-
-	int tmp;
+	Array<int> tmp2(size);
 
 	try
 	{
+		int tmp;
 		i[1] = 2;
 		tmp = i[1];
 		std::cout << tmp << std::endl;
-		std::cout << i.size() << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	try
+	{
+		for (int b = 0; b < size; b++)
+		{
+			tmp2[b] = static_cast<int>(i[b]);
+			std::cout << "copy assign = " << tmp2[b] << std::endl;
+		}
 	}
 	catch (const std::exception &e)
 	{

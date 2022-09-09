0afe57b89474b7ab7de3bee9594fe48bf8f3acd7
vctrubio <vctrubio@gmail.com>
2022-09-09 21:05:47 +0200

    08 question on this->c.begin (why container::c) bitiches?¿?¿

--- 42challenge/08/ex02/Mutantstack.hpp
+++ 42challenge/08/ex02/Mutantstack.hpp
@@ -1,23 +1,44 @@
-#ifndef	MUTANTSTACK_HPP 
+#ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
-
 
 #include <stack>
 #include <iostream>
+#include <vector>
 
 /*
-deque 
-vectors 
+deque
+vectors
 list
-
-
-
 */
-class MutantStack
+
+template <typename X>
+class MutantStack : public std::stack<X>
 {
-
-
+public:
+	typedef typename std::stack<X>::container_type::iterator iter;
+	MutantStack()
+	{
+	}
+	MutantStack(const MutantStack<X> &old)
+	{
+		*this = old;
+	}
+	MutantStack<X> &operator=(const MutantStack<X> &old)
+	{
+		this->c = old.c;
+		return (*this);
+	}
+	~MutantStack()
+	{
+	}
+	iter begin(void)
+	{
+		return (this->c.begin());
+	}
+	iter end(void)
+	{
+		return (this->c.end());
+	}
 };
-
-
+//CONFUSED about where the c variable comes from (containter_type but wtf)
 #endif
--- 42challenge/08/ex02/main.cpp
+++ 42challenge/08/ex02/main.cpp
@@ -10,14 +10,32 @@ pop_back
 
 int main()
 {
-	std::stack<int> mystack;
+    MutantStack<int> mstack;
 
-	mystack.push(22);
-	mystack.push(22);
-	while (!mystack.empty())
-	{
-			std::cout << mystack.top() << std::endl;
-			mystack.pop();
-	}
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+
+    MutantStack<int>::iter it = mstack.begin();
+    MutantStack<int>::iter ite = mstack.end();
+    ++it;
+    --it;
+
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack);
+
 
 };

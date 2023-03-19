fbfc9370e5e8fc2ce32809ad7a6c347a546b9003
vctrubio <vctrubio@gmail.com>
2023-03-19 17:09:41 +0100

    ex00gtp

--- 42challenge/09/00/BitcoinExchange.hpp
+++ 42challenge/09/00/BitcoinExchange.hpp
@@ -5,31 +5,48 @@
 
 class BitcoinExchange
 {
-	std::map<time_t, long> prices;
+	std::map<time_t, float> prices;
+
 public:
 	BitcoinExchange();
 	BitcoinExchange(const BitcoinExchange &oldBitcoinExchange);
-	BitcoinExchange& operator= (const BitcoinExchange &oldBitcoinExchange);
+	BitcoinExchange &operator=(const BitcoinExchange &oldBitcoinExchange);
 	~BitcoinExchange();
 
-   void add_price(time_t date, long price) {
-        prices[date] = price;
-    }
+	void add_price(time_t date, float price)
+	{
+		prices[date] = price;
+	}
 
-    long get_price(time_t date) const {
-        std::map<time_t, long>::const_iterator it = prices.find(date);
-        if (it == prices.end()) {
-            return 0; // or some other default value
-        }
-        return it->second;
-    }
+	float get_price(time_t date) const
+	{
+		std::map<time_t, float>::const_iterator it = prices.find(date);
+		if (it == prices.end())
+		{
+			return 0;
+		}
+		return it->second;
+	}
 
-    void print_all_prices() const {
-        std::map<time_t, long>::const_iterator it;
-        for (it = prices.begin(); it != prices.end(); ++it) {
-            std::cout << "Date: " << std::ctime(&it->first) << "Price: " << it->second << std::endl;
-        }
-    }
+	void print_all_prices() const
+	{
+		char buffer[80];
+		std::map<time_t, float>::const_iterator it;
+		std::time_t timestamp;
+		std::tm *tm;
+
+		std::cout << "Date | Value" << std::endl;
+		for (it = prices.begin(); it != prices.end(); ++it)
+		{
+			if (it->second > 0 && it->second < 1001)
+			{
+				timestamp = it->first;
+				tm = std::localtime(&timestamp);
+				std::strftime(buffer, sizeof(buffer), "%Y-%m-%d", tm);
+				std::cout << buffer << " | " << it->second << std::endl;
+			}
+		}
+	}
 };
 
 #endif
